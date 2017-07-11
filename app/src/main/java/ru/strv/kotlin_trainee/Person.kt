066c370fb052c89cfad38d917246a46dfc436263
saturov <saturov@surfstudio.ru>
2017-07-11 17:48:59 +0300

    Сеттеры и геттеры проперти

--- app/src/main/java/ru/strv/kotlin_trainee/Person.kt
+++ app/src/main/java/ru/strv/kotlin_trainee/Person.kt
@@ -1,9 +1,12 @@
 package ru.strv.kotlin_trainee
 
 class Person(name: String, surname: String) : Animal(name) {
-    init {
 
-    }
+    var name: String = ""
+        get () = field.toUpperCase()
+        set(value) {
+            field = "Name : $value"
+        }
 
     fun add(x: Int, y: Int): Int = x + y
 }
