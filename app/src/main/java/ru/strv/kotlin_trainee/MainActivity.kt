683bc24c6339217bb3b08e3a65fb009e3f2d72bd
saturov <saturov@surfstudio.ru>
2017-07-11 10:21:27 +0300

    Аттач адаптера

--- app/src/main/java/ru/strv/kotlin_trainee/MainActivity.kt
+++ app/src/main/java/ru/strv/kotlin_trainee/MainActivity.kt
@@ -9,6 +9,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val items = listOf("Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     fun initRecyclerView() {
         val forecastList = forecast_list as RecyclerView
         forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
     }
 
     fun toast(message: String, length: Int = Toast.LENGTH_SHORT) {
